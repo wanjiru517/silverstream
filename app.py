9b766ef31ae3242d23bb063ab4e7dc39842f541e
henry mugai <106584669+HenryMugai@users.noreply.github.com>
2022-06-29 10:25:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from http.cookies import Morsel
 import streamlit as st
 import pandas as pd
 st.header('THE SILVERSTREAM ACADEMY')
-st.text('LOCATED WITHIN THE HIGHLANDS OF EMBU COUNTY IS A PRODUCTIVE ENVIRONMENT SUITABLE AND FRIENDLY ENVIRONMENT THAT ENABLES YUR CHILD TO GROW AND BECOME PRODUCTIVE IN MANY AREAS')
 st.image('photos/Dash Team.PNG')
 
 st.text_area('LOCATED WITHIN THE HIGHLANDS OF EMBU COUNTY IS A PRODUCTIVE ENVIRONMENT SUITABLE AND FRIENDLY ENVIRONMENT THAT ENABLES YUR CHILD TO GROW AND BECOME PRODUCTIVE IN MANY AREAS')
@@ -13,3 +12,5 @@ locations= {"lat":[-0.467277],
             "lon":[37.450611]}
 df = pd.DataFrame(data=locations)
 st.map(df)            
+st.header('school info'
+         )
