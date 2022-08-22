da76f9b559beca7d278f6c1e0083ad2706f8b18e
Henry Mugai <henrymugai36@gmail.com>
2022-08-22 21:23:48 +0300

    commit

--- app.py
+++ app.py
@@ -14,8 +14,12 @@ st.markdown(
 st.image('st 2.jpeg')
 st.sidebar.write ('KCPE RESULTS')
  # Load exel data from user
-pd.read_csv("C:\Users\USER\silver stream\silverstream\KCPE RESULTS.2018.csv")
-
+sheet1, sheet2, sheet3, sheet4 = None, None, None, None
+with pd.ExcelFile("C:\Users\USER\silver stream\silverstream\KCPE RESULTS..xlsx") as reader:
+    sheet1 = pd.read_excel(reader, sheet_name='Sheet1')
+    sheet2 = pd.read_excel(reader, sheet_name='Sheet2')
+    sheet3 = pd.read_excel(reader, sheet_name='Sheet3')
+    sheet4 = pd.read_excel(reader, sheet_name='Sheet4')
 locations= {"lat":[-0.467277], 
             "lon":[37.450611]}
 df = pd.DataFrame(data=locations)
