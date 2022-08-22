132bf1e2664732d3568fc276c62c58bbcf513b58
Henry Mugai <henrymugai36@gmail.com>
2022-08-22 22:07:00 +0300

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from ast import keyword
 import csv
 import matplotlib
 from http.cookies import Morsel
@@ -15,11 +16,8 @@ st.image('st 2.jpeg')
 st.sidebar.write ('KCPE RESULTS')
  # Load exel data from user
 
-with pd.ExcelFile(r"C:\Users\USER\silver stream\silverstream\KCPE RESULTS..xlsx") as reader:
-    sheet1 = pd.read_excel(reader, sheet_name='Sheet1')
-    sheet2 = pd.read_excel(reader, sheet_name='Sheet2')
-    sheet3 = pd.read_excel(reader, sheet_name='Sheet3')
-    sheet4 = pd.read_excel(reader, sheet_name='Sheet4')
+excel_file = 'KCPE RESULTS.xlxs'
+keyword = pd.read_excel(excel_file)
 locations= {"lat":[-0.467277], 
             "lon":[37.450611]}
 df = pd.DataFrame(data=locations)
