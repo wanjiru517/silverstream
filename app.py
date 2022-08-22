d674059616b885f94bc904d924bdc68adc2794ce
Henry Mugai <henrymugai36@gmail.com>
2022-08-22 20:23:22 +0300

    commit

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ if st.button('more info'):
     st.write('contact 0716731548')
 st.sidebar.write ('KCPE RESULTS')
  # Load data from desktop
-load = pd.read_excel('C:\Users\USER\Desktop\KCPE RESULTS..xlsx')
+load = pd.read_excel(r'C:\Users\USER\Desktop\KCPE RESULTS..xlsx')
