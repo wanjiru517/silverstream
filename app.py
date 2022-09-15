578ceb5fd1c481ed47972f62640aae8dea92d98e
Your Name <you@example.com>
2022-09-16 00:22:25 +0300

    commit

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 
-from email import header
-from importlib.metadata import files
-from msilib.schema import File
+#from lib2to3.pytree import _Resultd
 from pkgutil import get_data
 from unicodedata import name
 from http.cookies import Morsel
 from unittest import result
-
 import streamlit as st
-import streamlit.components.v1 as components
-from secrets import choice
 import pandas as pd
-from pandas import read_excel
+from pandas import ExcelFile
 import plotly as px
 #---Main Page---
 st.set_page_config(page_title='THE SILVERSTREAM ACADEMY')
@@ -20,25 +15,23 @@ st.title('KCPE results analysis')
 st.image('st 1.jpeg')
 
 ### --- LOAD DATAFRAME
-df = pd.read_excel("C:\\Users\\USER\\silver stream\\silverstream\\KCPE RESULTS.xlsx")
-print(df.head())
-## excel file KCSE RESULTS.xlsx
+
 df_2018 = pd.read_excel('KCPE RESULTS.xlsx', sheet_name='2018')
 df_2019 = pd.read_excel('KCPE RESULTS.xlsx', sheet_name='2019')
 df_2020 = pd.read_excel('KCPE RESULTS.xlsx', sheet_name='2020')
 df_2021 = pd.read_excel('KCPE RESULTS.xlsx', sheet_name='2021')
 # Combining all datasets
 pdList = [df_2018, df_2019,  df_2020, df_2021]
-all_records = pd.concat(pdList)
-     
+all_records = pd.concat(pdList)   
 
 st.markdown(
     'A simpe school dashboard outputing the school,s results since founding')
-
+st.image('st 2.jpeg')
+st.write("Sample performance")
 #----SIDEBAR-----
 st.sidebar.header('Filter Here')
 #use selectbox to select one option at a time
-options = st.sidebar._selectbox("select the sheet:",[2018,2019,2020,2021,'all'])
+options = st.sidebar.selectbox("select the sheet:",[2018,2019,2020,2021, 'all'])
 
 if options == 2018:
     st.write(df_2018)
@@ -49,11 +42,13 @@ elif options == 2020:
 elif options == 2021:
     st.write(df_2021)
 else:
-    st.write(all_records)   
-#read data from 2019
+    st.write(all_records)
+            
+# #---KCSE RESULTS PER YEAR[BAR CHART]----
+# result_by_year = (df_selection.groupby(by=["2018"])
+# )
 
-df_2019 = pd.read_excel('KCPE RESULTS.xlsx', sheet_name='2019')        
-#---KCSE RESULTS [BAR CHART]----
+# result_by_year = (df_selection.groupby)
 
 
  
