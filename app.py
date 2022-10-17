09f897798f55b85bcde05ddac42a7d6e8dece487
Your Name <you@example.com>
2022-10-17 11:31:38 +0300

    commit

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
 #from lib2to3.pytree import _Resultd
 from ast import If
+from asyncore import write
 from optparse import Values
 from os import rename
 from pkgutil import get_data
+from re import M
 from tkinter.font import names
 from unicodedata import name
 from http.cookies import Morsel
@@ -32,7 +34,8 @@ df_2021 = pd.read_excel('KCPE RESULTS.xlsx', sheet_name='2021')
 # Combining all datasets
 pdList = [df_2018, df_2019,  df_2020, df_2021]
 all_records = pd.concat(pdList)   
-
+cols = [1, 2, 3, 4, 5, 6, 7]
+df = pdList
 
 st.markdown(
     'The dashboard shows KCPE RESULTS as from 2018 to present')
@@ -40,7 +43,7 @@ df_2018.head()
 st.write("They are as follows,")
 #----SIDEBAR-----
 st.sidebar.header('Filter Here')
-#use selectbox to select one option at a time
+#selectbox to select one option at a time
 options = st.sidebar.selectbox("select the sheet:",[2018,2019,2020,2021, 'all'])
 
 if options == 2018:
@@ -55,19 +58,27 @@ else:
     st.write(all_records)
             
 # #---RESULTS BY SUBJECT--
-df_2018.plot(x="ENG", y="KIS", kind="box")
-plp.show() 
 
-plp.scatter(df_2018['TOTAL'], df_2019['TOTAL'])
+options = st.sidebar.selectbox("select the subject:",[1, 2, 3, 4, 5, 6, 7])
 
-var = pd.read_excel('KCPE RESULTS.xlsx', sheet_name='2018')
-print(var)
-x = list(var['ENG'])
-y = list(var['KIS'])
+if options == 1:
+    st.write(1)
+elif options == 2:
+    st.write(2)
+elif options == 3:
+    st.write(3)
+elif options == 4:
+    st.write(4)
+elif options == 5:
+    st.write(5)
+elif options == 6:
+    st.write(6)
+elif options == 7:
+    st.write(7)
 
-plp.figure(figsize=(10,10))
-plp.style.use('seaborn')
-plp.scatter(x,y,marker="*",s=100,edgecolors="black",c="yellow")
-plp.title("Excel sheet to Scatter Plot")
-plp.show()
-
+#---results per gender---
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["M"].unique(),
+    default=df["F"].unique()
+)
